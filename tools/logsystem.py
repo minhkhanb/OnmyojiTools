0b581b257b8a3097518a5acd0d90b139fd368bb1
Marc <minhkhanb@gmail.com>
2021-06-16 20:52:16 +0700

    translate to en logger utf8

--- tools/logsystem.py
+++ tools/logsystem.py
@@ -8,11 +8,11 @@ class MyLog():
 
     @staticmethod
     def init():
+        stream = logging.FileHandler(filename='log.log', mode='a', encoding='utf-8').stream
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)s %(filename)-17s [line:%(lineno)-3s] %(levelname)-7s %(name)-9s %(message)s',
                             datefmt='%Y %b %d %H:%M:%S',
-                            filename='log.log',
-                            filemode='w')
+                            stream=stream)
 
         #################################################################################################
         #定义一个StreamHandler，将INFO级别或更高的日志信息打印到标准错误，并将其添加到当前的日志处理对象#
